4cb4db3e12ad18fafd5d8983ddd9306af8bfca00
psy337337 <psy337337@naver.com>
2025-05-31 16:20:58 +0900

    [Fix] 하둡 설치 패키지 버전 수정

--- installHadoop.sh
+++ installHadoop.sh
@@ -3,9 +3,9 @@
 
 # install hadoop & make symbolic link
 cd /home/hadoop
-wget https://downloads.apache.org/hadoop/common/hadoop-3.3.4/hadoop-3.3.4.tar.gz
-tar xzf hadoop-3.3.4.tar.gz
-ln -s /home/hadoop/hadoop-3.3.4 ~/hadoop;
+wget https://downloads.apache.org/hadoop/common/hadoop-3.4.1/hadoop-3.4.1.tar.gz
+tar xzf hadoop-3.4.1.tar.gz
+ln -s /home/hadoop/hadoop-3.4.1 ~/hadoop;
 
 # Setting hadoop's environment variables
 cat ./HadoopInstallTool/config_templates/bashrc.txt >> /home/hadoop/.bashrc
