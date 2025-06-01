cf52ea986afb16160bf806f74460f5471553240c
psy337337 <psy337337@naver.com>
2025-06-01 10:43:02 +0900

    [Test] DataNode 셋팅 부분 까지만  주석해제

--- 1.sh
+++ 1.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-# if [ $# -eq 0 ];then
-#     echo "Please Input ip address"
-#     exit 0
-# fi
+if [ $# -eq 0 ];then
+    echo "Please Input ip address"
+    exit 0
+fi
 
-# ./HadoopInstallTool/hostset.sh $@
-# ./HadoopInstallTool/makeUser.sh
+./HadoopInstallTool/hostset.sh $@
+./HadoopInstallTool/makeUser.sh
 
-# sudo -S bash -c 'echo "hadoop ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/hadoop && chmod 440 /etc/sudoers.d/hadoop'
+sudo -S bash -c 'echo "hadoop ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/hadoop && chmod 440 /etc/sudoers.d/hadoop'
 
-# #first time can't used sshpass
+#first time can't used sshpass
 
-# num=0
-# address=""
-# for i in $@
-# do
-#     num=$(($num+1))
-#     if [ "$(($num%2))" == "0" ]; then
-#         address=$i
-# 	continue
-#     elif [ "$num" == "1" ]; then
-#         continue
-#     fi
-#     sshpass -p $i ssh ubuntu@$address -o StrictHostKeyChecking=no "echo ubuntu | sudo -S apt-get install -y git" 
-#     sshpass -p $i ssh ubuntu@$address -o StrictHostKeyChecking=no "git clone https://github.com/psy337337/HadoopInstallTool.git && \
-#                                                                                     echo ubuntu | sudo -S chmod +x ./HadoopInstallTool/*.sh && \
-#                                                                                     echo ubuntu | sudo -S ./HadoopInstallTool/hostset.sh $@ && \
-#                                                                                     echo ubuntu | sudo -S ./HadoopInstallTool/makeUser.sh && \
-#         echo ubuntu | sudo -S bash -c 'echo \"hadoop ALL=(ALL) NOPASSWD:ALL\" > /etc/sudoers.d/hadoop && chmod 440 /etc/sudoers.d/hadoop'"
-#     sshpass -p $i ssh ubuntu@$address -o StrictHostKeyChecking=no "echo ubuntu | sudo ./HadoopInstallTool/install.sh" 
-# done
+num=0
+address=""
+for i in $@
+do
+    num=$(($num+1))
+    if [ "$(($num%2))" == "0" ]; then
+        address=$i
+	continue
+    elif [ "$num" == "1" ]; then
+        continue
+    fi
+    sshpass -p $i ssh ubuntu@$address -o StrictHostKeyChecking=no "echo ubuntu | sudo -S apt-get install -y git" 
+    sshpass -p $i ssh ubuntu@$address -o StrictHostKeyChecking=no "git clone https://github.com/psy337337/HadoopInstallTool.git && \
+                                                                                    echo ubuntu | sudo -S chmod +x ./HadoopInstallTool/*.sh && \
+                                                                                    echo ubuntu | sudo -S ./HadoopInstallTool/hostset.sh $@ && \
+                                                                                    echo ubuntu | sudo -S ./HadoopInstallTool/makeUser.sh && \
+        echo ubuntu | sudo -S bash -c 'echo \"hadoop ALL=(ALL) NOPASSWD:ALL\" > /etc/sudoers.d/hadoop && chmod 440 /etc/sudoers.d/hadoop'"
+    sshpass -p $i ssh ubuntu@$address -o StrictHostKeyChecking=no "echo ubuntu | sudo ./HadoopInstallTool/install.sh" 
+done
 
 # sshpass -p hadoop ssh hadoop@hdn -o StrictHostKeyChecking=no -t "cd; git clone https://github.com/psy337337/HadoopInstallTool.git &&\
 #                         echo hadoop | sudo -S chmod +x ./HadoopInstallTool/*.sh &&\
@@ -55,6 +55,6 @@
 
 # echo "hadoop" | su - hadoop -c "cd; ./HadoopInstallTool/installHadoop.sh; source ~/.bashrc;"
 
-./HadoopInstallTool/inputProfile.sh
+# ./HadoopInstallTool/inputProfile.sh
 
-sshpass -p hadoop ssh hadoop@hdn -o StrictHostKeyChecking=no -t "cd ~; pwd; source ~/.bashrc; ./HadoopInstallTool/setHadoop.sh"
+# sshpass -p hadoop ssh hadoop@hdn -o StrictHostKeyChecking=no -t "cd ~; pwd; source ~/.bashrc; ./HadoopInstallTool/setHadoop.sh"
